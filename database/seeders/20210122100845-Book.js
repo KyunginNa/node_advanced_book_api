0d48cf189d0cd2a29bcec68f72134e0c22c6bcde
Kyungin Na <nakyungin93@gmail.com>
2021-01-22 11:16:04 +0100

    Adding sequelize, creating and migrating Books table

---
+++ database/seeders/20210122100845-Book.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const { query } = require('express')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Books', [
+      {
+        title: 'Learn NodeJS with Thomas',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: 'Learn Sequelize with Thomas',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {},
+}
